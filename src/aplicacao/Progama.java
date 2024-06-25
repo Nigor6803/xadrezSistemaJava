68eb04aaad59a69c4172efa9827ad4c496c5ba73
Igor Nascimento <nigor6803@gmail.com>
2024-06-25 17:07:38 -0300

    Board feito

--- src/aplicacao/Progama.java
+++ src/aplicacao/Progama.java
@@ -1,5 +1,6 @@
 package aplicacao;
 
+import tabuleiro.Board;
 import tabuleiro.Posicao;
 
 public class Progama {
@@ -7,7 +8,8 @@ public class Progama {
 	public static void main(String[] args) {
 		
 		
-		Posicao pos = new  Posicao(3,5);
+		Posicao pos = new Posicao(3,5);
+		Board boa = new Board(8,8);
 		System.out.println(pos);
 
 	}
---
+++ src/tabuleiro/Board.java
@@ -0,0 +1,33 @@
+package tabuleiro;
+
+public class Board {
+	
+	private Integer linha;
+	private Integer coluna;
+	private Peca[][]pecas;
+	
+	public Board() {
+		}
+	
+	public Board(Integer linha, Integer coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+		pecas = new Peca[linha][coluna];
+	}
+	
+	public Integer getLinha() {
+		return linha;
+	}
+	
+	public void setLinha(Integer linha) {
+		this.linha = linha;
+	}
+	
+	public Integer getColuna() {
+		return coluna;
+	}
+	
+	public void setColuna(Integer coluna) {
+		this.coluna = coluna;
+	}
+}
