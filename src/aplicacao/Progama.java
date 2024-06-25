2173f34e3127b723192032f1efe63661045dd9fd
Igor Nascimento <nigor6803@gmail.com>
2024-06-25 16:38:20 -0300

    Posição

--- src/aplicacao/Progama.java
+++ src/aplicacao/Progama.java
@@ -1,9 +1,14 @@
 package aplicacao;
 
+import tabuleiro.Posicao;
+
 public class Progama {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		Posicao pos = new  Posicao(3,5);
+		System.out.println(pos);
 
 	}
 
