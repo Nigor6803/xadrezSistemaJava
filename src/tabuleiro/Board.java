65ad9580d9f17ad343ecfedb027071b43162328e
Igor Nascimento <nigor6803@gmail.com>
2024-07-02 10:28:45 -0300

    tabuleiro de erros

--- src/tabuleiro/Board.java
+++ src/tabuleiro/Board.java
@@ -1,46 +1,66 @@
 package tabuleiro;
 
 public class Board {
-	
-	private Integer linha;
-	private Integer coluna;
-	private Peca[][]pecas;
-	
+
+	private Integer linhas;
+	private Integer colunas;
+	private Peca[][] pecas;
+
 	public Board() {
-		}
-	
+	}
+
 	public Board(Integer linha, Integer coluna) {
-		this.linha = linha;
-		this.coluna = coluna;
+		if (linha < 1 || coluna < 1) {
+			throw new ExceçãoTabuleiro("Erro: Deve haver pelo menos 1 linha e 1 coluna");
+		}
+
+		this.linhas = linha;
+		this.colunas = coluna;
 		pecas = new Peca[linha][coluna];
 	}
-	
+
 	public Integer getLinha() {
-		return linha;
+		return linhas;
 	}
-	
-	public void setLinha(Integer linha) {
-		this.linha = linha;
-	}
-	
+
 	public Integer getColuna() {
-		return coluna;
+		return colunas;
 	}
-	
-	public void setColuna(Integer coluna) {
-		this.coluna = coluna;
-	}
-	
+
 	public Peca peca(Integer linha, Integer coluna) {
+		if (!posicaoexistente(linha,coluna)) {
+			throw new ExceçãoTabuleiro("POSIÇÃO INCORRETA");
+		}
 		return pecas[linha][coluna];
 	}
-	
+
 	public Peca peca(Posicao posicao) {
+		if (!posicaoexistente(posicao)) {
+			throw new ExceçãoTabuleiro("POSIÇÃO INCORRETA");
+		}
 		return pecas[posicao.getLinha()][posicao.getColuna()];
 	}
-	
+
 	public void lugarDaPeca(Peca peca, Posicao posicao) {
+		if (temUmaPeca(posicao)) {
+			throw new ExceçãoTabuleiro("Já existe uma peça em posição " + posicao);
+		}
 		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
 		peca.posicao = posicao;
+	}
+
+	private boolean posicaoexistente(int linha, int coluna) {
+		return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+	}
+
+	public boolean posicaoexistente(Posicao posicao) {
+		return posicaoexistente(posicao.getLinha(), posicao.getColuna());
+	}
+
+	public boolean temUmaPeca(Posicao posicao) {
+		if (!posicaoexistente(posicao)) {
+			throw new ExceçãoTabuleiro("POSIÇÃO INCORRETA");
+		}
+		return peca(posicao) != null;
 	}
 }
---
+++ src/tabuleiro/ExceçãoTabuleiro.java
@@ -0,0 +1,11 @@
+package tabuleiro;
+
+public class ExceçãoTabuleiro extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ExceçãoTabuleiro(String msg) {
+		super(msg);
+	}
+	
+}
